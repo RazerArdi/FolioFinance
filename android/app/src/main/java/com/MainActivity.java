2ab3c98f0db7e63925fd43f723edeb0937080133
bayua <bayuardiyansyah30@gmail.com>
2024-12-02 01:24:36 +0700

    Bug - Forced Close (java)

---
+++ android/app/src/main/java/com/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.foliofinance;  // Must match your application's package
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
