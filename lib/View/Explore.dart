7d78f9a6a2ce95fd58614301286ab764ef404b8d
bayua <bayuardiyansyah30@gmail.com>
2024-12-02 13:18:16 +0700

    V0.6.1 - Permission handler geolocator dan pop up untuk mengarah ke Google Maps

--- lib/View/Explore.dart
+++ lib/View/Explore.dart
@@ -3,20 +3,101 @@ import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:get/get.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:url_launcher/url_launcher.dart';
 
+class Explore extends StatefulWidget {
+  const Explore({Key? key}) : super(key: key);
 
-// Explore Widget
-class Explore extends StatelessWidget {
+  @override
+  _ExploreState createState() => _ExploreState();
+}
+
+class _ExploreState extends State<Explore> {
   final CompanyController companyController = Get.put(CompanyController());
   final TextEditingController _searchController = TextEditingController();
+  final MapController _mapController = MapController();
+  LatLng? initialCenter;
+  LatLng? userLocation;
+  bool isLoadingLocation = true;
 
-  Explore({Key? key}) : super(key: key);
+  @override
+  void initState() {
+    super.initState();
+    _getCurrentLocation();
+  }
+
+  Future<void> _getCurrentLocation() async {
+    try {
+      LocationPermission permission = await Geolocator.checkPermission();
+      if (permission == LocationPermission.denied) {
+        permission = await Geolocator.requestPermission();
+      }
+
+      if (permission == LocationPermission.whileInUse || permission == LocationPermission.always) {
+        Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+        setState(() {
+          initialCenter = LatLng(position.latitude, position.longitude);
+          userLocation = LatLng(position.latitude, position.longitude);
+          isLoadingLocation = false;
+        });
+      } else {
+        Get.snackbar('Permission Denied', 'Location permission is required to show your location on the map.');
+        setState(() {
+          isLoadingLocation = false;
+        });
+      }
+    } catch (e) {
+      Get.snackbar('Error', 'Unable to retrieve location');
+      setState(() {
+        isLoadingLocation = false;
+      });
+    }
+  }
+
+  void _launchGoogleMaps(LatLng location) async {
+    final String googleMapsUrl =
+        'https://www.google.com/maps/search/?api=1&query=${location.latitude},${location.longitude}';
+
+    try {
+      if (await canLaunchUrl(Uri.parse(googleMapsUrl))) {
+        await launchUrl(Uri.parse(googleMapsUrl));
+      } else {
+        Get.snackbar('Error', 'Could not launch Google Maps');
+      }
+    } catch (e) {
+      Get.snackbar('Error', 'An error occurred: $e');
+    }
+  }
+
+  void _centerOnUserLocation() {
+    if (userLocation != null) {
+      _mapController.move(userLocation!, 10.0);
+    } else {
+      Get.snackbar('Location Unavailable', 'Unable to find your current location');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Explore'),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 8.0),
+            child: IconButton(
+              icon: const Icon(Icons.my_location, color: Colors.white),
+              tooltip: 'Center on My Location',
+              onPressed: _centerOnUserLocation,
+              style: IconButton.styleFrom(
+                backgroundColor: Colors.blue.shade600,
+                shape: const CircleBorder(),
+                padding: const EdgeInsets.all(10),
+              ),
+            ),
+          ),
+        ],
       ),
       body: Column(
         children: [
@@ -43,8 +124,11 @@ class Explore extends StatelessWidget {
               final filteredCompanies = companyController.filteredCompanies;
 
               return FlutterMap(
+                mapController: _mapController,
                 options: MapOptions(
-                  initialCenter: LatLng(37.7749, -122.4194), // Default center
+                  initialCenter: initialCenter ?? LatLng(32.377716, -86.300568),
+                  initialZoom: 10.0,
+                  maxZoom: 18.0,
                   minZoom: 5.0,
                   onTap: (tapPosition, point) =>
                   companyController.selectedCompany.value = null,
@@ -55,6 +139,7 @@ class Explore extends StatelessWidget {
                     "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
                     subdomains: ['a', 'b', 'c'],
                   ),
+                  // Company Markers
                   MarkerLayer(
                     markers: filteredCompanies.map((company) {
                       return Marker(
@@ -72,6 +157,60 @@ class Explore extends StatelessWidget {
                       );
                     }).toList(),
                   ),
+                  // User Location Marker
+                  if (userLocation != null)
+                    MarkerLayer(
+                      markers: [
+                        Marker(
+                          width: 50.0,
+                          height: 50.0,
+                          point: userLocation!,
+                          child: GestureDetector(
+                            onTap: () {
+                              // Show user location details
+                              showDialog(
+                                context: context,
+                                builder: (BuildContext context) {
+                                  return AlertDialog(
+                                    title: const Text('My Location'),
+                                    content: Column(
+                                      mainAxisSize: MainAxisSize.min,
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        Text('Latitude: ${userLocation!.latitude}'),
+                                        Text('Longitude: ${userLocation!.longitude}'),
+                                        const SizedBox(height: 10),
+                                        GestureDetector(
+                                          onTap: () => _launchGoogleMaps(userLocation!),
+                                          child: Text(
+                                            'Open in Google Maps',
+                                            style: TextStyle(
+                                              color: Colors.blue,
+                                              decoration: TextDecoration.underline,
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    actions: [
+                                      TextButton(
+                                        onPressed: () => Navigator.of(context).pop(),
+                                        child: const Text('Close'),
+                                      ),
+                                    ],
+                                  );
+                                },
+                              );
+                            },
+                            child: const Icon(
+                              Icons.person_pin_circle,
+                              size: 50,
+                              color: Colors.blue,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                 ],
               );
             }),
